ae323919d2208e2df4782048dc57524b05b9f645
Joseph Martin <josephmartin1350@gmail.com>
2022-01-28 13:44:13 -0700

    commit tree 988a03d54e06b93bbc5c956fbada155589303d7a

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+// "https://via.placeholder.com/400"
+
+function PlantCard({ plant }) {
+  const [isInStock, setIsInStock] = useState(true)
+
+  function handleInStockClick() {
+    setIsInStock(!isInStock)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: $ {plant.price}</p>
+      {isInStock ? (
+        <button className="primary" onClick={handleInStockClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleInStockClick}>Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState('')
+
+  const plantsToDisplay = plants.filter(plant => 
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(r => r.json())
+    .then(plants => setPlants(plants))
+  }, [])
+
+  function handleNewPlantSubmit(newPlant) {
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+  }
+
+  function handleSearchChange(value) {
+    setSearch(value)
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onNewPlantSubmit={handleNewPlantSubmit} />
+      <Search search={search} onSearchChange={handleSearchChange} />
+      <PlantList plants={plantsToDisplay} />
     </main>
   );
 }
